f74923f38c8a2981a462a5b8ddb2f4d5bde210c6
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2016-08-01 15:20:43 -0600

    mayor resumido

--- report/asistente_diario_reporte.py
+++ report/asistente_diario_reporte.py
@@ -6,8 +6,10 @@ class asistente_diario_reporte(osv.osv_memory):
     _name = 'l10n_gt_extra.asistente_diario_reporte'
     _columns = {
         'folio_inicial': fields.integer('Folio inicial', required=True),
-        'diarios_id': fields.many2many('account.journal', 'diario_diario_rel', 'reporte_id', 'diario_id', 'Diarios', required=True),
+        'diarios_id': fields.many2many('account.journal', 'diario_diario_rel', 'reporte_id', 'diario_id', 'Diarios'),
+        'diarios_resumidos_id': fields.many2many('account.journal', 'diario_diario_resumido_rel', 'reporte_id', 'diario_id', 'Diarios resumidos'),
         'periodos_id': fields.many2many('account.period', 'diario_periodo_rel', 'reporte_id', 'periodo_id', 'Periodos', required=True),
+        'tipo': fields.selection([ ('diario','Diario'), ('mayor','Mayor') ], string='Tipo', required=True),
     }
 
     _defaults = {
