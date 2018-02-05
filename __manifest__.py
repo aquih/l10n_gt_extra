9ff76067e79f22ffd94398e7fd726f24dc3328c8
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2018-02-05 23:17:14 +0000

    Version 11

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Guatemala - Reportes y funcionalidad extra',
+    'version': '1.0',
+    'category': 'Localization',
+    'description': """ Reportes requeridos por la SAT y otra funcionalidad extra para llevar un contabilidad en Guatemala. """,
+    'author': 'José Rodrigo Fernández Menegazzo',
+    'website': 'http://solucionesprisma.com/',
+    'depends': ['l10n_gt', 'account_tax_python', 'account_cancel'],
+    'data': [
+        'data/l10n_gt_extra_base.xml',
+        'views/account_view.xml',
+        'views/res_partner_view.xml',
+        'views/report.xml',
+        'views/reporte_banco.xml',
+        'views/reporte_partida.xml',
+        'views/reporte_compras.xml',
+        'views/reporte_ventas.xml',
+    ],
+    'demo': [],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
