7a6aa2148a62162337d21f09dc8f9ba7e07b9f2e
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2018-08-09 17:34:36 +0000

    En el reporte de ventas mostrar rangos

--- report/reporte_ventas.py
+++ report/reporte_ventas.py
@@ -44,10 +44,12 @@ class ReporteVentas(models.AbstractModel):
 
             numero = f.number or f.numero_viejo or '-',
 
+            # Por si es un diario de rango de facturas
+            if f.journal_id.facturas_por_rangos:
+                numero = f.name
+
             # Por si usa factura electrónica
             if 'firma_gface' in f.fields_get() and f.firma_gface:
-                numero = f.name
-            if f.journal_id.facturas_por_rangos:
                 numero = f.name
 
             # Por si usa tickets
