2a0467c7c9de95bfba701040348cacdaaf081df2
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2019-05-02 15:42:50 +0000

    Opcion para no validar nits

--- models/res_partner.py
+++ models/res_partner.py
@@ -2,18 +2,25 @@
 
 from openerp import models, fields, api, _
 from openerp.exceptions import UserError, ValidationError
+import logging
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+    no_validar_nit = fields.Boolean(string="No validar NIT")
+
     @api.multi
     @api.constrains('vat')
     def _validar_nit(self):
+        logging.warn('validar_nit')
         for p in self:
             if p.vat == 'CF' or p.vat == 'C/F' or not p.vat:
                 return True
 
             if p.country_id and p.country_id.code != 'GT':
+                return True
+
+            if p.no_validar_nit:
                 return True
 
             nit = p.vat.replace('-','')
@@ -31,16 +38,17 @@ class ResPartner(models.Model):
             resultante = ( 11 - ( total % 11 ) ) % 11
 
             if str(resultante) != verificador:
-                raise ValidationError("El NIT no es correcto (según lineamientos de la SAT)")
+                raise ValidationError("El NIT " + p.vat + " no es correcto (según lineamientos de la SAT)")
 
     @api.multi
     @api.constrains('vat')
     def _validar_duplicado(self):
+        logging.warn('validar_duplicado')
         for p in self:
-            if not p.parent_id and p.vat and p.vat != 'CF' and p.vat != 'C/F':
+            if not p.parent_id and p.vat and p.vat != 'CF' and p.vat != 'C/F' and not p.no_validar_nit:
                 repetidos = p.search([('vat','=',p.vat), ('id','!=',p.id), ('parent_id','=',False)])
                 if len(repetidos) > 0:
-                    raise ValidationError("El NIT ya existe")
+                    raise ValidationError("El NIT " + p.vat + " ya existe")
 
     @api.model
     def name_search(self, name, args=None, operator='ilike', limit=100):
