fafd65fc9af665008549608ec60a75d5dde53468
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2020-01-08 20:09:24 +0000

    Error en saldo final

--- report/reporte_mayor.py
+++ report/reporte_mayor.py
@@ -43,9 +43,9 @@ class ReporteMayor(models.AbstractModel):
         accounts_str = ','.join([str(x) for x in datos['cuentas_id']])
         if datos['agrupado_por_dia']:
             self.env.cr.execute('select a.id, a.code as codigo, a.name as cuenta, l.date as fecha, t.include_initial_balance as balance_inicial, sum(l.debit) as debe, sum(l.credit) as haber ' \
-            	'from account_move_line l join account_account a on(l.account_id = a.id)' \
-            	'join account_account_type t on (t.id = a.user_type_id)' \
-            	'where a.id in ('+accounts_str+') and l.date >= %s and l.date <= %s group by a.id, a.code, a.name,l.date, t.include_initial_balance ORDER BY l.date,a.code',
+                'from account_move_line l join account_account a on(l.account_id = a.id)' \
+                'join account_account_type t on (t.id = a.user_type_id)' \
+                'where a.id in ('+accounts_str+') and l.date >= %s and l.date <= %s group by a.id, a.code, a.name, l.date, t.include_initial_balance ORDER BY a.code',
             (datos['fecha_desde'], datos['fecha_hasta']))
 
             for r in self.env.cr.dictfetchall():
@@ -64,29 +64,31 @@ class ReporteMayor(models.AbstractModel):
                 }
                 lineas.append(linea)
 
-            for l in lineas:
-                if not l['balance_inicial']:
-                    l['saldo_inicial'] += self.retornar_saldo_inicial_inicio_anio(l['id'], datos['fecha_desde'])
-                    l['saldo_final'] += l['saldo_inicial'] + l['debe'] - l['haber']
-                    totales['saldo_inicial'] += l['saldo_inicial']
-                    totales['saldo_final'] += l['saldo_final']
-                else:
-                    l['saldo_inicial'] += self.retornar_saldo_inicial_todos_anios(l['id'], datos['fecha_desde'])
-                    l['saldo_final'] += l['saldo_inicial'] + l['debe'] - l['haber']
-                    totales['saldo_inicial'] += l['saldo_inicial']
-                    totales['saldo_final'] += l['saldo_final']
-
             cuentas_agrupadas = {}
             llave = 'codigo'
             for l in lineas:
                 if l[llave] not in cuentas_agrupadas:
-                    cuentas_agrupadas[l[llave]] = {'codigo': l[llave],'cuenta':l['cuenta'] ,'saldo_inicial':l['saldo_inicial'],'saldo_final':l['saldo_final'],'fechas':[], 'total_debe':0, 'total_haber':0}
+                    cuentas_agrupadas[l[llave]] = {
+                        'codigo': l[llave],
+                        'cuenta': l['cuenta'],
+                        'saldo_inicial': 0,
+                        'saldo_final': 0,
+                        'fechas': [],
+                        'total_debe': 0,
+                        'total_haber': 0
+                    }
+
+                    if not l['balance_inicial']:
+                        cuentas_agrupadas[l[llave]]['saldo_inicial'] = self.retornar_saldo_inicial_inicio_anio(l['id'], datos['fecha_desde'])
+                    else:
+                        cuentas_agrupadas[l[llave]]['saldo_inicial'] = saldo = self.retornar_saldo_inicial_todos_anios(l['id'], datos['fecha_desde'])
                 cuentas_agrupadas[l[llave]]['fechas'].append(l)
 
             for cuenta in cuentas_agrupadas.values():
                 for fecha in cuenta['fechas']:
                     cuenta['total_debe'] += fecha['debe']
                     cuenta['total_haber'] += fecha['haber']
+                cuenta['saldo_final'] += cuenta['saldo_inicial'] + cuenta['total_debe'] - cuenta['total_haber']
 
             lineas = cuentas_agrupadas.values()
         else:
