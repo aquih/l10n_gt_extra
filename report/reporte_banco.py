45b4438bb81bb3907d7d7bb9c797d103189021f2
rocketgithub <rodrigo@solucionesprisma.com>
2021-02-02 10:15:22 -0600

    Movimientos validados para el balance también

--- report/reporte_banco.py
+++ report/reporte_banco.py
@@ -56,7 +56,7 @@ class ReporteBanco(models.AbstractModel):
         return lineas
 
     def balance_inicial(self, datos):
-        self.env.cr.execute('select coalesce(sum(debit) - sum(credit), 0) as balance, coalesce(sum(amount_currency), 0) as balance_moneda from account_move_line where account_id = %s and date < %s', (datos['cuenta_bancaria_id'][0], datos['fecha_desde']))
+        self.env.cr.execute('select coalesce(sum(debit) - sum(credit), 0) as balance, coalesce(sum(amount_currency), 0) as balance_moneda from account_move_line where account_id = %s and parent_state = %s and date < %s', (datos['cuenta_bancaria_id'][0], 'posted', datos['fecha_desde']))
         return self.env.cr.dictfetchall()[0]
 
     @api.model
