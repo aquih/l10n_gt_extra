579e2f783238d6f40ec2e0068bae8952dbc89179
rborstgithub <56568136+rborstgithub@users.noreply.github.com>
2023-10-09 12:31:36 -0600

    Reporte inventario v.16
    https://www.aquih.com/web#id=15882&cids=1&model=helpdesk.ticket&view_type=form

--- report/asistente_reporte_inventario.py
+++ report/asistente_reporte_inventario.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.release import version_info
 from odoo.exceptions import UserError, ValidationError
 import time
 
@@ -8,16 +9,22 @@ class AsistenteReporteInventario(models.TransientModel):
     _name = 'l10n_gt_extra.asistente_reporte_inventario'
 
     def _default_cuenta(self):
-        if len(self.env.context.get('active_ids', [])) > 0:
-            return self.env.context.get('active_ids')
+        if version_info[0] in [13, 14, 15]:
+            if len(self.env.context.get('active_ids', [])) > 0:
+                return self.env.context.get('active_ids')
+            else:
+                return self.env['account.account'].search([]).ids
         else:
-            return self.env['account.account'].search([]).ids
+            return []
 
     cuentas_id = fields.Many2many("account.account", string="Diario", required=True, default=_default_cuenta)
     folio_inicial = fields.Integer(string="Folio Inicial", required=True, default=1)
     fecha_hasta = fields.Date(string="Fecha Final", required=True, default=lambda self: time.strftime('%Y-%m-%d'))
 
     def print_report(self):
+        if not self.cuentas_id:
+            raise UserError('Debe ingresar las cuentas que serán utilizadas en el reporte')
+
         data = {
              'ids': [],
              'model': 'l10n_gt_extra.asistente_reporte_inventario',
