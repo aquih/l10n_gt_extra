86979ebe7bc63c9fe1ec74b5440aa8c65a38ed63
rocketgithub <rodrigo@aquih.com>
2023-11-14 22:40:03 -0600

    Usar el balance correcto en la función también

--- report/reporte_banco.py
+++ report/reporte_banco.py
@@ -31,11 +31,11 @@ class ReporteBanco(models.AbstractModel):
                     detalle['credito'] = -1 * linea.amount_currency
 
             # Si la cuenta no tiene moneda o la moneda de la cuenta es la misma de la compañía
-            if not cuenta.currency_id or (cuenta.currency_id.id == linea.company_id.currency_id.id):
+            if not cuenta.currency_id or (cuenta.currency_id.id == cuenta.company_id.currency_id.id):
                 usar_balance_moneda = False
             
                 # Se agregan lineas que no tiene moneda o tienen la misma moneda que la compañía
-                if not linea.currency_id or linea.currency_id.id == linea.company_id.currency_id.id:
+                if not linea.currency_id or linea.currency_id.id == cuenta.company_id.currency_id.id:
                     lineas.append(detalle)
                     
             # Si no, si la cuenta si tienen moneda y la moneda de la cuenta es diferente que la de la compañía
@@ -59,8 +59,20 @@ class ReporteBanco(models.AbstractModel):
         return lineas
 
     def balance_inicial(self, datos):
+        cuenta = self.env['account.account'].browse(datos['cuenta_bancaria_id'][0])
+        
+        # Si la cuenta no tiene moneda o la moneda de la cuenta es la misma de la compañía
+        if not cuenta.currency_id or (cuenta.currency_id.id == cuenta.company_id.currency_id.id):
+            usar_balance_moneda = False        
+        # Si no, si la cuenta si tienen moneda y la moneda de la cuenta es diferente que la de la compañía
+        else:
+            usar_balance_moneda = True
+
         self.env.cr.execute('select coalesce(sum(debit) - sum(credit), 0) as balance, coalesce(sum(amount_currency), 0) as balance_moneda from account_move_line where account_id = %s and parent_state = %s and date < %s', (datos['cuenta_bancaria_id'][0], 'posted', datos['fecha_desde']))
-        return self.env.cr.dictfetchall()[0]
+        result = self.env.cr.dictfetchall()[0]
+        result['usar_balance_moneda'] = usar_balance_moneda
+        
+        return result
 
     @api.model
     def _get_report_values(self, docids, data=None):
