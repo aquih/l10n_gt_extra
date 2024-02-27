5c9943e7e17a62262c8a280f6acc5406401a3c5f
rocketgithub <rodrigo@aquih.com>
2024-02-27 11:37:36 -0600

    Quitar atributo que ya no existe

--- report/asistente_reporte_mayor.py
+++ report/asistente_reporte_mayor.py
@@ -27,7 +27,7 @@ class AsistenteReporteMayor(models.TransientModel):
     fecha_desde = fields.Date(string="Fecha Inicial", required=True, default=lambda self: time.strftime('%Y-%m-01'))
     fecha_hasta = fields.Date(string="Fecha Final", required=True, default=lambda self: time.strftime('%Y-%m-%d'))
     name = fields.Char('Nombre archivo', size=32)
-    archivo = fields.Binary('Archivo', filters='.xls')
+    archivo = fields.Binary('Archivo')
 
     def print_report(self):
         if not self.cuentas_id:
