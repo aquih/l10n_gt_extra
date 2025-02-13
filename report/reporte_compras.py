ab1c126f54c3063bdac4d0f77e995291cf27a51e
rocketgithub <rodrigo@aquih.com>
2025-02-13 11:26:01 -0600

    No tomar en cuenta facturas con valor 0

--- report/reporte_compras.py
+++ report/reporte_compras.py
@@ -24,6 +24,7 @@ class ReporteCompras(models.AbstractModel):
             ('journal_id','in',journal_ids),
             ('date','<=',datos['fecha_hasta']),
             ('date','>=',datos['fecha_desde']),
+            ('amount_total','!=',0),
         ]
         
         if 'type' in self.env['account.move'].fields_get():
