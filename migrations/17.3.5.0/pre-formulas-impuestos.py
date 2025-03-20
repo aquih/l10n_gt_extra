90ca0215563572715b972fa07d42c8cc4e3d9b01
rocketgithub <rodrigo@aquih.com>
2025-03-20 15:23:28 -0600

    Por si existen varios impuestos por ser multicompañía

--- migrations/17.3.5.0/pre-formulas-impuestos.py
+++ migrations/17.3.5.0/pre-formulas-impuestos.py
@@ -6,6 +6,6 @@ _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     def migrate(cr, version):
-    cr.execute("update account_tax set python_compute = 'base * 0.005' where id in (select res_id from ir_model_data where name = '1_impuestos_plantilla_timbre_prensa')")
-    cr.execute("update account_tax set python_compute = '(base > 30000) and (30000 * -0.05 + (base - 30000) * -0.07) or base * -0.05' where id in (select res_id from ir_model_data where name = '1_impuestos_plantilla_isr_retencion')")
+    cr.execute("update account_tax set formula = 'base * 0.005' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_timbre_prensa')")
+    cr.execute("update account_tax set formula = '(base > 30000) and (30000 * -0.05 + (base - 30000) * -0.07) or base * -0.05' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_isr_retencion')")
     _logger.info("Formulas de impuestos")
