07e30f6fe455704620b741eee076e81cf758bc2b
Rodrigo Fernández <rodrigo@aquih.com>
2025-04-01 18:41:28 +0000

    Más ajustes para versión 18.0. Y aún faltán más ¯\_(ツ)_/¯

--- migrations/17.3.5.0/pre-formulas-impuestos.py
+++ migrations/17.3.5.0/pre-formulas-impuestos.py
@@ -6,6 +6,11 @@ _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
     def migrate(cr, version):
-    cr.execute("update account_tax set formula = 'base * 0.005' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_timbre_prensa')")
-    cr.execute("update account_tax set formula = '(base > 30000) and (30000 * -0.05 + (base - 30000) * -0.07) or base * -0.05' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_isr_retencion')")
-    _logger.info("Formulas de impuestos")
+    cr.execute("update account_tax set formula = 'base/1.12 * 0.005', price_include_override = 'tax_excluded' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_timbre_prensa')")
+    cr.execute("update account_tax set formula = '(base/1.12 > 30000) and (30000 * -0.05 + (base/1.12 - 30000) * -0.07) or base/1.12 * -0.05', price_include_override = 'tax_excluded' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_isr_retencion')")
+    cr.execute("update account_tax set price_include_override = 'tax_included' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_idp_disel')")
+    cr.execute("update account_tax set price_include_override = 'tax_included' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_idp_regular')")
+    cr.execute("update account_tax set price_include_override = 'tax_included' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_idp_super')")
+    cr.execute("update account_tax set price_include_override = 'tax_excluded' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_isr_factura_especial')")
+    cr.execute("update account_tax set price_include_override = 'tax_excluded' where id in (select res_id from ir_model_data where name like '%impuestos_plantilla_iva_especial')")
+    _logger.info("Formulas y precios incluidos de impuestos")
