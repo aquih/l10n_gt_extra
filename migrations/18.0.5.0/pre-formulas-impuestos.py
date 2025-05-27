000a080ee7842bb7bb908b8191facd02d1410a62
rocketgithub <rodrigo@aquih.com>
2025-05-26 21:23:43 -0600

    Definición extra erronea

--- migrations/18.0.5.0/pre-formulas-impuestos.py
+++ migrations/18.0.5.0/pre-formulas-impuestos.py
@@ -5,7 +5,6 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    def migrate(cr, version):
     cr.execute("update account_tax set formula = 'base/1.12 * 0.005', price_include_override = 'tax_excluded' where id in (select res_id from ir_model_data where name like '%impuestos%_timbre_prensa')")
     cr.execute("update account_tax set formula = '(base/1.12 > 30000) and (30000 * -0.05 + (base/1.12 - 30000) * -0.07) or base/1.12 * -0.05', price_include_override = 'tax_excluded' where id in (select res_id from ir_model_data where name like '%impuestos%_isr_retencion')")
     cr.execute("update account_tax set price_include_override = 'tax_included' where id in (select res_id from ir_model_data where name like '%impuestos%_idp_disel')")
