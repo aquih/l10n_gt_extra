af18f20f2a69e70848792bd91bf0d7138eea3cab
rocketgithub <rodrigo@aquih.com>
2025-06-03 12:48:41 -0600

    Otra vista vieja

--- migrations/17.0.4.0/pre-borrar-vistas.py
+++ migrations/17.0.4.0/pre-borrar-vistas.py
@@ -11,6 +11,7 @@ def migrate(cr, version):
     util.records.remove_view(cr, xml_id="l10n_gt_extra.product_template_form_view")
     util.records.remove_view(cr, xml_id="l10n_gt_extra.l10n_gt_extra_product_variant_easy_edit_view")
     util.records.remove_view(cr, xml_id="l10n_gt_extra.l10n_gt_view_res_partner_form")
+    util.records.remove_view(cr, xml_id="l10n_gt_extra.l10n_gt_view_partner_form")
     util.records.remove_view(cr, xml_id="l10n_gt_extra.l10n_gt_extra_product_template_tree_view")
     util.records.remove_view(cr, xml_id="l10n_gt_extra.l10n_gt_extra_product_template_tree_view")
     _logger.info("Vistas viejas borradas")
