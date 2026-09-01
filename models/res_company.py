e215e69170e07e357404e10c0e2f41f108f19854
rocketgithub <rodrigo@aquih.com>
2026-09-01 11:45:54 -0600

    Mejorar carga de impuestos

--- models/res_company.py
+++ models/res_company.py
@@ -4,7 +4,15 @@ from odoo.exceptions import ValidationError
 class ResCompany(models.Model):
     _inherit = "res.company"
     
-    def update_gt_taxes(self):
+    def cargar_impuestos(self):
         for company in self:
             Template = self.env['account.chart.template'].with_company(company)
-            Template._load_data({'account.tax': Template._get_gt_extra_account_tax()})
+            impuestos = Template._get_gt_extra_account_tax()
+            Template._load_data({'account.tax': impuestos})
+
+    def cargar_impuestos_globales(self):
+        for company in self:
+            Template = self.env['account.chart.template'].with_company(company)
+            impuestos = Template._get_gt_extra_account_tax()
+            impuestos_globales = {k: impuestos[k] for k in ['impuestos_plantilla_iva_retencion_global', 'impuestos_plantilla_isr_retencion_global'] if k in impuestos}
+            Template._load_data({'account.tax': impuestos_globales})
